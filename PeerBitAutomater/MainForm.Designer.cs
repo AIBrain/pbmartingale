6cc9bd1cf0d620701e3794cb2484231954a677ae
unknown <dwmcqueen@gmail.com>
2013-02-11 20:29:06 -0600

    Added new methods and tested code.

---
+++ PeerBitAutomater/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PeerBitAutomater
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.lbl24HourProfit = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Balance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "24 Hour Net Profit:";
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.AutoSize = true;
+            this.lblBalance.Location = new System.Drawing.Point(205, 123);
+            this.lblBalance.MaximumSize = new System.Drawing.Size(100, 13);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(22, 13);
+            this.lblBalance.TabIndex = 6;
+            this.lblBalance.Text = "0.0";
+            // 
+            // lbl24HourProfit
+            // 
+            this.lbl24HourProfit.AutoSize = true;
+            this.lbl24HourProfit.Location = new System.Drawing.Point(205, 145);
+            this.lbl24HourProfit.MaximumSize = new System.Drawing.Size(100, 13);
+            this.lbl24HourProfit.Name = "lbl24HourProfit";
+            this.lbl24HourProfit.Size = new System.Drawing.Size(22, 13);
+            this.lbl24HourProfit.TabIndex = 7;
+            this.lbl24HourProfit.Text = "0.0";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(82, 28);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(139, 20);
+            this.tbUsername.TabIndex = 8;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(82, 54);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(139, 20);
+            this.tbPassword.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "User:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Pass:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 176);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lbl24HourProfit);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PB Martingale";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.Label lbl24HourProfit;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PeerBitAutomater/MainForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters;
+using System.Threading;
+
+namespace PeerBitAutomater
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            PeerBetAPI pbAPI = new PeerBetAPI(tbUsername.Text, tbPassword.Text);
+
+            this.lbl24HourProfit.Text = pbAPI.TodayProfitFromPB.ToString();
+            this.lblBalance.Text = pbAPI.Balance.ToString();
+            //pbAPI.LoginPeerBet();
+            String lowestOrderID = "";
+            Double lowstOrderPrice = 0.0;
+
+            int count = 1;
+
+            pbAPI.GetLowestInstantHalfChance(ref lowestOrderID, ref lowstOrderPrice);
+
+            //Loop thru martingale
+            do{  
+                pbAPI.PlaceOrder(lowestOrderID, 1);
+
+                Thread.Sleep(1000);
+                if (pbAPI.CheckIfWonOrder(lowestOrderID) == true)
+                {
+                    pbAPI.GetLowestInstantHalfChance(ref lowestOrderID, ref lowstOrderPrice);
+                    count++;
+                }
+                else
+                {
+                    Double foundOrderPrice = 0.0;
+                    lowstOrderPrice = lowstOrderPrice * 2;
+                    pbAPI.GetClosestOrderInstantDouble(lowstOrderPrice, ref lowestOrderID, ref foundOrderPrice);
+                }
+            }
+            while (count < 10);
+                        //pbAPI.GetClosestOrderInstantDouble(Double.Parse(this.textBox1.Text), ref lowestOrderID, ref lowstOrderPrice);
+            //this.textBox1.Text = pbAPI.APIKey;//single string
+            pbAPI.RefreshPeerBetGetUserInfo();
+            this.lbl24HourProfit.Text = pbAPI.TodayProfitFromPB.ToString();
+            this.lblBalance.Text = pbAPI.Balance.ToString();
+         /*       String strGetARafflesResponse = pbRequest.GetPBResponse("method=getactiveraffles");
+ 
+                var results = JsonConvert.DeserializeObject<dynamic>(strGetARafflesResponse, new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.All,
+                    TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple
+                });
+
+            foreach (var results2 in results)
+            {
+                String raffleID = results2.raffle_id;
+                //Console.WriteLine(results2.raffle_id);
+            }
+            */
+                /*Dictionary<string, string> c2 = JsonConvert.DeserializeObject<Dictionary<string, string>>(strGetARafflesResponse, new JsonSerializerSettings
+          {
+              TypeNameHandling = TypeNameHandling.All,
+              TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple
+          });*/
+            /*finalUrl = string.Format("{0}{1}", "https://peerbet.org/api.php", "?" + "method=getactiveraffles");
+            
+             * request = WebRequest.Create(finalUrl);
+
+            response = request.GetResponse();
+            // Get the stream containing all content returned by the requested server.
+            dataStream = response.GetResponseStream();
+
+            // Open the stream using a StreamReader for easy access.
+            reader = new StreamReader(dataStream);
+            //92b0929a9caea4f8352cd7693bfb13b1
+            // Read the content fully up to the end.
+            responseFromServer = reader.ReadToEnd();
+
+            // Clean up the streams.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            ArrayList al = (ArrayList)JsonConvert.DeserializeObject<ArrayList>(responseFromServer);
+            ht = (Hashtable)JsonConvert.DeserializeObject<Hashtable>(responseFromServer); */
+        }
+			//catch(Exception ex)
+			//{
+			//	MessageBox.Show(this, "Error Deserializing: " + ex.Message, "Deserialization Error");
+			//}
+
+//            request.GetResponse()
+        //}
+    }
+}
